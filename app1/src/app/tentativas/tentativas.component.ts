78a0afaae8e0e73975fc21a413fad012aa0e9fa1
Rigote <matheus.rigote@rakuten.com.br>
2018-03-13 19:37:11 -0300

    Finalizando jogo de inglês.

--- app1/src/app/tentativas/tentativas.component.ts
+++ app1/src/app/tentativas/tentativas.component.ts
@@ -9,28 +9,27 @@ import { Coracao } from '../shared/coracao.model'
 })
 export class TentativasComponent implements OnInit, OnChanges {
 
-  @Input() public tentativas: number
+  @Input() public tentativas: number;
 
   public coracoes: Coracao[] = [
     new Coracao(true),
     new Coracao(true),
     new Coracao(true)
-  ]
+  ];
 
   constructor() {
-    
+
   }
 
   ngOnChanges() {
-    if(this.tentativas !== this.coracoes.length) {
-      let indice = this.coracoes.length - this.tentativas
-      this.coracoes[indice - 1].cheio = false
+    if (this.tentativas !== this.coracoes.length) {
+      let indice = this.coracoes.length - this.tentativas;
+      this.coracoes[indice - 1].cheio = false;
     }
-    console.log('Tentativas recebidas do painel', this.tentativas)
   }
 
   ngOnInit() {
-    
+
   }
 
 }
